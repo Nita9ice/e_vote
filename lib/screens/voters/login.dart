70a996727ab666901c37a35de22dd38768429acc
Nita9ice <erhonsele1@gmail.com>
2025-08-06 15:13:52 +0100

    intro

---
+++ lib/screens/voters/login.dart
@@ -0,0 +1,113 @@
+import 'package:e_vote/components/widgets/button.dart';
+import 'package:e_vote/components/widgets/text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // Controller for handling input in the email text field
+  final TextEditingController usernameController = TextEditingController();
+
+  // Controller for handling input in the password text field
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/images/background2.jpg'),)
+            ),
+            child: Column(
+              
+              children: [
+                // space
+                SizedBox(height: 241,),
+                // Main text 
+                Text('Welcome Back'),
+                    
+                // space
+                SizedBox(height: 22,),
+                    
+                // sub text
+                Text('Login'),
+                    
+                // space
+                SizedBox(height: 40,),
+                    
+                // Text field for user 
+                MyTextField(controller: usernameController, 
+                hintText: 'Username;'),
+                    
+                // space
+                SizedBox(height: 42,),
+                    
+                MyTextField(controller: passwordController, 
+                hintText: 'Password:'),
+                    
+                // space
+                SizedBox(height: 14,),
+                    
+                // Forgot password
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          // Navigate to forgot password screen
+                          Navigator.pushNamed(context, '/forgot');
+                        },
+                        child: Text(
+                          'Forgot Password?',
+                          
+                        ),
+                      ),
+                    ),
+                    
+                     // space
+                SizedBox(height: 61,),
+                    
+                // login button
+                MyButton(buttonText: 'Login', onPressed: (){}),
+                    
+                SizedBox(height: 8,),
+                    
+                 // Create account text
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Don\'t have an account?',
+                          
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/signup');
+                          },
+                          child: Text(
+                            'Sign up',
+                            
+                          ),
+                        ),
+                      ],
+                    ),
+                    
+                    
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-/// AppTheme is a central class that defines both light and dark themes
-/// for the entire e-Voting mobile application.
+// The app theme class define the theme data for the app
 class AppTheme {
+
   // ------------------ LIGHT THEME ------------------
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light, // Sets the base brightness mode to light
@@ -14,12 +14,14 @@ class AppTheme {
     primaryColor: const Color.fromRGBO(26, 35, 126, 1), // Deep Blue
 
   );
+
+
   // ------------------ DARK THEME ------------------
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark, // Dark mode base brightness
 
     // Scaffold background for dark mode
-    scaffoldBackgroundColor: const Color.fromRGBO(18, 18, 18, 1), // Very dark background
+    scaffoldBackgroundColor: const Color.fromRGBO(18, 18, 18, 1), 
 
     // Primary color stays the same (Deep Blue)
     primaryColor: const Color.fromRGBO(26, 35, 126, 1),
