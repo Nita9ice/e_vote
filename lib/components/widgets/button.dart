92733313a7bc3ac492b71aa59a202ef6cb98c4e1
Nita9ice <erhonsele1@gmail.com>
2025-08-13 10:11:26 +0100

    onpressed

--- lib/components/widgets/button.dart
+++ lib/components/widgets/button.dart
@@ -21,30 +21,33 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      padding: EdgeInsets.symmetric(vertical: 14, horizontal: 94),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          colors: [
-
-          Color.fromRGBO(3, 58, 202, 1),
+    return  GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 14, horizontal: 94),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            colors: [
+      
+            Color.fromRGBO(3, 58, 202, 1),
+            
+            Color.fromRGBO(132, 166, 255, 1)
+            
+          ]
           
-          Color.fromRGBO(132, 166, 255, 1)
+          )
           
-        ]
-        
-        )
-        
-      ),
-      child: Text(buttonText,
-     style: TextStyle(
-       fontFamily: 'Roboto',
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Color.fromRGBO(255, 255, 255, 1,)
-                ),
+        ),
+        child: Text(buttonText,
+       style: TextStyle(
+         fontFamily: 'Roboto',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromRGBO(255, 255, 255, 1,)
+                  ),
+        ),
       ),
     );
   }
